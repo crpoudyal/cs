7d98a63c5e2afa6166b173c19eff84bea3328652
crpoudyal <crpoudyal@gmail.com>
2021-11-27 17:39:06 +0545

    Generics Method in c#

---
+++ Advance c#/GenericMethod.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AdvanceCsharp
+{
+    class Gen
+    {
+        public void sub<T>(T x, T y)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            dynamic sum = a + b;
+            Console.WriteLine("Sum : {0} ", sum);
+        }
+
+    }
+    class GenericMethod : Gen
+    {
+        static void Main()
+        {
+            GenericMethod gm = new GenericMethod();
+            gm.sub<int>(25.90, 63);
+
+        }
+
+    }
+}
